72f4caa2c75f19cc591b3a1ca08d341a352fc56c
Sam Maister <goosewobbler@protonmail.com>
2025-12-30 19:13:43 +0000

    test: fix remaining unit issues

--- packages/electron-service/test/bridge.spec.ts
+++ packages/electron-service/test/bridge.spec.ts
@@ -67,7 +67,7 @@ describe('ElectronCdpBridge', () => {
   });
 
   beforeEach(() => {
-    vi.restoreAllMocks();
+    vi.clearAllMocks();
   });
   describe('connect', async () => {
     const expectedContextId = 999;
